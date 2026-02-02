020233b8e8e756cd254ffb55920fad491040db30
sr-857 <146226597+sr-857@users.noreply.github.com>
2026-02-03 04:15:41 +0530

    fix: resolve broken SDG logo by using a local image

--- src/components/SDGLogo.tsx
+++ src/components/SDGLogo.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export function SDGLogo({ className = "h-6 w-6" }: { className?: string }) {
   return (
-    <img 
-      src="https://raw.githubusercontent.com/un-stats/sdg-logos/master/en/sdg-en-goal-12.png" 
-      alt="SDG 12: Responsible Consumption and Production" 
+    <img
+      src="/images/sdg12.png"
+      alt="SDG 12: Responsible Consumption and Production"
       className={className}
     />
   );
